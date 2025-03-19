0252c35d41ba7f5692b47d848453b0d8534497ef
madhuk54 <krishnashetty98765@gmail.com>
2025-03-19 10:55:51 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,25 +1,18 @@
-package Project;
-
-import java.util.Scanner;
-
 public class Factorial {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number (N) to generate factorials up to N: ");
-        int number = scanner.nextInt();
-        scanner.close();
-
-        System.out.println("Factorial Numbers up to " + number + ":");
-        for (int i = 0; i <= number; i++) {
-            System.out.println(i + "! = " + calculateFactorial(i));
+        if (args.length == 0) {
+            System.out.println("Please provide a number as a command-line argument.");
+            return;
         }
-    }
-
-    public static long calculateFactorial(int n) {
-        long fact = 1;
-        for (int i = 2; i <= n; i++) {
+        
+        int n = Integer.parseInt(args[0]); // Get input from command line
+        int fact = 1;
+        
+        for (int i = 1; i <= n; i++) {
             fact *= i;
         }
-        return fact;
+        
+        System.out.println("Factorial of " + n + " is " + fact);
     }
 }
+
